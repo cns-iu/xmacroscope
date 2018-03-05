ed90158fd0432f14c0c2cc31bd0765691c6effdd
Bruce Herr II <bherr@indiana.edu>
2018-03-05 14:57:06 -0500

    Fix merge conflict

--- packages/aisl/src/app/dino-core/index.ts
+++ packages/aisl/src/app/dino-core/index.ts
@@ -2,7 +2,3 @@ export { DinoCoreModule } from './dino-core.module';
 export { Changes, DatumId, isDatumId } from './shared/changes';
 export { IField, Field } from './shared/field';
 export { FieldProcessor } from './shared/field-processor';
-<<<<<<< .merge_file_dEOsaK
-=======
-export { StreamCache } from './shared/stream-cache';
->>>>>>> .merge_file_2Frn7K
