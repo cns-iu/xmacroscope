642153cfb4dfadccd52abe1e2d0da10e66cc8bc0
Bruce Herr II <bherr@indiana.edu>
2018-03-07 16:47:23 -0500

    Removed mockup module Removed package.json start-X commands. We will use routes in the near future instead.

--- packages/aisl/src/app/mav-mockup/material.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatSidenavModule, MatRadioModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatTabsModule } from '@angular/material';
-
-@NgModule({
-  imports: [ MatButtonModule, MatSidenavModule, MatRadioModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatTabsModule ],
-  exports: [ MatButtonModule, MatSidenavModule, MatRadioModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatTabsModule ],
-})
-export class MaterialModule { }
--- packages/aisl/src/app/shared/shared.module.ts
+++ packages/aisl/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule, MatTabsModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
-  imports: [ CommonModule, MatTableModule, MatSidenavModule, MatTabsModule, MatToolbarModule ],
-  exports: [ CommonModule, MatTableModule, MatSidenavModule, MatTabsModule, MatToolbarModule ],
+  imports: [ BrowserAnimationsModule, CommonModule, MatTableModule, MatSidenavModule, MatTabsModule, MatToolbarModule ],
+  exports: [ BrowserAnimationsModule, CommonModule, MatTableModule, MatSidenavModule, MatTabsModule, MatToolbarModule ],
   declarations: []
 })
 export class SharedModule { }
