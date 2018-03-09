13fd558cba01863c6fb46b30d53e90ce204281a1
Gagandeep Singh <singhgag@indiana.edu>
2018-03-09 16:23:24 -0500

    Routing + minor sass tweaks

---
+++ packages/aisl/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent as home } from './aisl-home/home/home.component';
+import { HomeComponent as backend } from './aisl-backend/home/home.component';
+import { HomeComponent as mav } from './aisl-mav/home/home.component';
+import { HomeComponent as endline } from './aisl-endline/home/home.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: home },
+  { path: 'backend', component: backend },
+  { path: 'mav', component: mav },
+  { path: 'endline', component: endline },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+}
