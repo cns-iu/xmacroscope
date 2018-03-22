f4002c331fa3f7f5cdaeba5bda7e9e33fb7b56f5
bryan kennedy <bkennedy@smm.org>
2018-03-22 10:57:58 -0500

    Comment style cleanup

--- packages/graphQLServer/src/graphql/resolvers/acl.js
+++ packages/graphQLServer/src/graphql/resolvers/acl.js
@@ -34,17 +34,13 @@ export const isAuthenticatedResolver = baseResolver
 // Extract the user and make sure they are an admin
 export const isAdminResolver = isAuthenticatedResolver
   .createResolver((root, args, { user }) => {
-    /*
-      If thrown, this error will bubble up to baseResolver's
-      error callback (if present).  If unhandled, the error is returned to
-      the client within the `errors` array in the response.
-    */
+    // If thrown, this error will bubble up to baseResolver's
+    // error callback (if present).  If unhandled, the error is returned to
+    // the client within the `errors` array in the response.
     if (!user.isAdmin) throw new ForbiddenError();
 
-    /*
-      Since we aren't returning anything from the
-      request resolver, the request will continue on
-      to the next child resolver or the response will
-      return undefined if no child exists.
-    */
+    // Since we aren't returning anything from the
+    // request resolver, the request will continue on
+    // to the next child resolver or the response will
+    // return undefined if no child exists.
   });
