52e1faff26ed0562cd742766b7e134d1ad36978f
bryan kennedy <bkennedy@smm.org>
2018-03-22 11:13:34 -0500

    Fixing webpack config issue
    Manually handle lerna node modules exclude.

--- packages/graphQLServer/webpack.common.js
+++ packages/graphQLServer/webpack.common.js
@@ -20,8 +20,15 @@ module.exports = {
   //
   // Externals ensures that webpack doesn't try to compile the node_modules
   // folder. Excluding the node_modules fixes problems with sequilize.
+  // We need an additional entry here that maps to the lerna monorepo
+  // node_modules folder, because nodeExternals won't walk the symlinks.
   //
-  externals: [nodeExternals({ whitelist: ['webpack/hot/poll?1000'] })],
+  externals: [
+    nodeExternals({ whitelist: ['webpack/hot/poll?1000'] }),
+    nodeExternals({
+      whitelist: ['webpack/hot/poll?1000'],
+      modulesDir: path.resolve(__dirname, '../../node_modules')
+    })],
 
   //
   // Use babel to transpile ES6 code to raw node.
