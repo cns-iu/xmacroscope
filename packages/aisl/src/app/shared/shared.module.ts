a06951243e0cf097e43ea8749e00ebc9122a8013
Gagandeep Singh <singhgag@indiana.edu>
2018-06-15 10:49:43 -0400

    initial theming for mav screen (WIP)

--- packages/aisl/src/app/shared/shared.module.ts
+++ packages/aisl/src/app/shared/shared.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule, MatTabsModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import {
+  MatTableModule,
+  MatTabsModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [ BrowserAnimationsModule, CommonModule, MatTableModule,
-            MatSidenavModule, MatTabsModule, MatToolbarModule
+            MatSidenavModule, MatTabsModule, MatToolbarModule,
+            MatGridListModule, MatButtonModule
           ],
   exports: [ BrowserAnimationsModule, CommonModule, MatTableModule,
-             MatSidenavModule, MatTabsModule, MatToolbarModule
+             MatSidenavModule, MatTabsModule, MatToolbarModule,
+             MatGridListModule, MatButtonModule
             ],
   declarations: []
 })
