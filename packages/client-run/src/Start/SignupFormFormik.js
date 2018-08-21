2563a6f5fc8b5977470aab0c4534f5b41c0032e3
bkennedy <bkennedy@smm.org>
2018-08-21 15:51:38 -0500

    Validate zipcode and generate location info
    Validate the zipcode and then use it to create the state and lat/long
    coordinates for the MAV screen.

--- packages/client-run/src/Start/SignupFormFormik.js
+++ packages/client-run/src/Start/SignupFormFormik.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withFormik } from 'formik';
 import { Mutation } from 'react-apollo';
+import zipcodes from 'zipcodes';
 import SignupForm from './SignupForm';
 import gql from 'graphql-tag';
 
@@ -14,20 +15,24 @@ const UPDATE_RUN_LOCAL = gql`
       handedness: $handedness
       siblings: $siblings
       zipcode: $zipcode
+      state: $state
+      latitude: $latitude
+      longitude: $longitude
     ) @client
   }
 `;
 
-// Wrap our form with the using withFormik HoC
 const SignupFormFormik = withFormik({
-  // Transform outer props into form values
-  mapPropsToValues: props => ({ name: '' }),
-
   // Add a custom validation function (this can be async too!)
   validate: (values, props) => {
     const errors = {};
     if (!values.name) {
       errors.name = 'Your name is required';
+    }
+    // Zip codes are 5 digits
+    // We don't accept the extra 4 digits.
+    if (!/(^\d{5}$)/.test(values.zipcode)) {
+      errors.zipcode = 'Please enter 5 numbers for a Zip Code.';
     }
     return errors;
   },
@@ -41,6 +46,7 @@ const SignupFormFormik = withFormik({
       setErrors /* setValues, setStatus, and other goodies */,
     },
   ) => {
+    const location = zipcodes.lookup(values.zipcode);
     props.updateRace({
       variables: {
         status: 'opponentSelect',
@@ -50,6 +56,9 @@ const SignupFormFormik = withFormik({
         handedness: values.handedness,
         siblings: values.siblings,
         zipcode: values.zipcode,
+        state: location.state,
+        latitude: location.latitude,
+        longitude: location.longitude,
       },
     });
   },
