40990b3d58937f590ac4a6f2c237ca916a2d634b
bkennedy <bkennedy@smm.org>
2018-10-20 15:47:31 -0500

    style: Change " to '
    Double quotes are our standard for js strings

--- packages/client-run/src/Primatives/TimerPrimatives.js
+++ packages/client-run/src/Primatives/TimerPrimatives.js
@@ -1,7 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const TimerWrapper = styled.div`
-  display: ${props => props.displayTimer ? "display" : "none"};
+  display: ${props => (props.displayTimer ? 'display' : 'none')};
   font-family: "Roboto Mono", monospace;
   font-weight: 700;
   font-size: 100px;
--- packages/client-run/src/Signup/personOptions.js
+++ packages/client-run/src/Signup/personOptions.js
@@ -1,37 +1,36 @@
 const personOptions = {};
 
-personOptions.shapes = ["square", "cross", "diamond", "triangle-up", "star"];
-
+personOptions.shapes = ['square', 'cross', 'diamond', 'triangle-up', 'star'];
 
 personOptions.colors = [
-  {name: "pastel red", hexValue:"#D36E44"},
-  {name: "red", hexValue:"#D30706"},
-  {name: "blue", hexValue:"#0707D2"},
-  {name: "purple", hexValue:"#8E1BCE"},
-  {name: "candy purple", hexValue:"#D372D2"},
-  {name: "aqua", hexValue:"#55D4D2"},
-  {name: "crimson red", hexValue:"#9B180F"},
-  {name: "forrest green", hexValue:"#33820D"},
-  {name: "honey brown", hexValue:"#D39827"},
-  {name: "neon yellow", hexValue:"#D3D406"}
+  { name: 'pastel red', hexValue: '#D36E44' },
+  { name: 'red', hexValue: '#D30706' },
+  { name: 'blue', hexValue: '#0707D2' },
+  { name: 'purple', hexValue: '#8E1BCE' },
+  { name: 'candy purple', hexValue: '#D372D2' },
+  { name: 'aqua', hexValue: '#55D4D2' },
+  { name: 'crimson red', hexValue: '#9B180F' },
+  { name: 'forrest green', hexValue: '#33820D' },
+  { name: 'honey brown', hexValue: '#D39827' },
+  { name: 'neon yellow', hexValue: '#D3D406' },
 ];
 
 // We're discussing the best way to categorize these on issue:
 // https://github.com/cns-iu/xmacroscope/issues/180
 // These were previously number range options.
-personOptions.ageGroups = ["Kid", "Pre-Teen", "Teen", "Adult", "Retired"];
+personOptions.ageGroups = ['Kid', 'Pre-Teen', 'Teen', 'Adult', 'Retired'];
 
-personOptions.gender = ["female", "male", "other"];
+personOptions.gender = ['female', 'male', 'other'];
 
 personOptions.favoriteActivity = [
-  "Sports",
-  "Cooking",
-  "Art",
-  "Gaming",
-  "Other"
+  'Sports',
+  'Cooking',
+  'Art',
+  'Gaming',
+  'Other',
 ];
 
-personOptions.handedness = ["left", "right"];
+personOptions.handedness = ['left', 'right'];
 
 personOptions.siblings = { min: 0, max: 12 };
 personOptions.height = { min: 36, max: 96 };
