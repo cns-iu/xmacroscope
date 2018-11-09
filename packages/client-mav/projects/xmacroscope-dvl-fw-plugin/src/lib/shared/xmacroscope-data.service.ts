58271a228bd10fdc0897570821254240c31aa1de
Bruce Herr II <bherr@indiana.edu>
2018-11-09 14:49:08 -0500

    One last tweak

--- packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/shared/xmacroscope-data.service.ts
+++ packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/shared/xmacroscope-data.service.ts
@@ -10,13 +10,13 @@ import { RunStreamController } from './run-stream-controller';
 @Injectable({providedIn: 'root'})
 export class XMacroscopeDataService {
   public readonly project: Project;
-  public readonly messageStream: Observable<Message>;
+  public readonly messages: Observable<Message>;
   public readonly runStreamController: RunStreamController;
 
   constructor() {
     const project = new XMacroscopeProject(true, false);
     this.project = project;
     this.runStreamController = project.runStreamController;
-    this.messageStream = this.runStreamController.messageStream;
+    this.messages = project.runStreamController.messageStream;
   }
 }
