b44c9f9525025fe9d60536cd770a677f3c3fb2ef
Bruce Herr II <bherr@indiana.edu>
2018-11-09 14:45:28 -0500

    refactored the xmac data service to make project a dvl Project

--- packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/shared/xmacroscope-data.service.ts
+++ packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/shared/xmacroscope-data.service.ts
@@ -1,17 +1,22 @@
+import { Project } from '@dvl-fw/core';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { XMacroscopeProject } from './xmacroscope-project';
 import { Message } from './message';
+import { RunStreamController } from './run-stream-controller';
 
 
 @Injectable({providedIn: 'root'})
 export class XMacroscopeDataService {
-  public readonly project: XMacroscopeProject;
+  public readonly project: Project;
   public readonly messageStream: Observable<Message>;
+  public readonly runStreamController: RunStreamController;
 
   constructor() {
-    this.project = new XMacroscopeProject(true, false);
-    this.messageStream = this.project.runStreamController.messageStream;
+    const project = new XMacroscopeProject(true, false);
+    this.project = project;
+    this.runStreamController = project.runStreamController;
+    this.messageStream = this.runStreamController.messageStream;
   }
 }
