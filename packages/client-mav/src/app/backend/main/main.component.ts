aa584a4308afd6d4f42fec952069ff261769c90c
Gagandeep Singh <singhgag@indiana.edu>
2018-11-11 14:35:24 -0500

    css skeleton for endline and backend with support for app header style toggle

--- packages/client-mav/src/app/backend/main/main.component.ts
+++ packages/client-mav/src/app/backend/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-backend-main',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.sass']
 })
 export class MainComponent implements OnInit {
+  showAppHeader = true;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    route.queryParams.subscribe((q) => {
+      if ('showAppHeader' in q) {
+        this.showAppHeader = q['showAppHeader'] === 'true' ? true : false;
+      } else if (Object.keys(q).length === 0) {
+        this.showAppHeader = true;
+      }
+    });
+  }
 
   ngOnInit() {
   }
