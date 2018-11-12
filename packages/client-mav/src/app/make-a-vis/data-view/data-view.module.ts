f7f1d07a6818def7ef7fd11bda61b91ac93ce4b4
Gagandeep Singh <singhgag@indiana.edu>
2018-11-11 20:08:10 -0500

    restart button on data-view + legend description text and css

--- packages/client-mav/src/app/make-a-vis/data-view/data-view.module.ts
+++ packages/client-mav/src/app/make-a-vis/data-view/data-view.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material';
+
 import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ],
   exports: [
     MainComponent
