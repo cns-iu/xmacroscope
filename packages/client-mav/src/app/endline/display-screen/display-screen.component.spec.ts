4b98386355f008548765995f8b2d2c699db575d2
Shashank Shekhar <shashekh@iu.edu>
2018-11-13 17:06:09 -0500

    added initial endline screen

---
+++ packages/client-mav/src/app/endline/display-screen/display-screen.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DisplayScreenComponent } from './display-screen.component';
+
+describe('DisplayScreenComponent', () => {
+  let component: DisplayScreenComponent;
+  let fixture: ComponentFixture<DisplayScreenComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DisplayScreenComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DisplayScreenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
