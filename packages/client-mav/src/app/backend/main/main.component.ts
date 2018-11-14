2cab48eae915bdd03885bd59affc1afa8aab76b1
Bruce Herr II <bherr@indiana.edu>
2018-11-14 09:45:54 -0500

    Implemented the debug screen

--- packages/client-mav/src/app/backend/main/main.component.ts
+++ packages/client-mav/src/app/backend/main/main.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Message, XMacroscopeDataService } from 'xmacroscope-dvl-fw-plugin';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
 @Component({
   selector: 'app-backend-main',
   templateUrl: './main.component.html',
@@ -8,8 +13,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MainComponent implements OnInit {
   showAppHeader = true;
+  messages: Observable<Message[]>;
+  private messageCache: Message[] = [];
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private dataService: XMacroscopeDataService) {
     route.queryParams.subscribe((q) => {
       if ('showAppHeader' in q) {
         this.showAppHeader = q['showAppHeader'] === 'true' ? true : false;
@@ -20,6 +27,24 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.messages = this.dataService.messages.pipe(
+      map<Message, Message[]>(msg => this.accumulate(msg))
+    );
   }
 
+  accumulate(message: Message): Message[] {
+    this.messageCache.push(message);
+    if (this.messageCache.length > this.dataService.runStreamController.historySize) {
+      this.messageCache.shift();
+    }
+    return this.messageCache.concat().reverse();
+  }
+
+  formatMsg(message: Message): string {
+    let msg = `type: ${message.type}, timestamp: ${message.timestamp.toLocaleString()}`;
+    if (message['run']) {
+      msg += `,<br>&nbsp;&nbsp;&nbsp;&nbsp;run: ${JSON.stringify(message['run'])}`;
+    }
+    return msg;
+  }
 }
