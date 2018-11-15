27784fa64ef8494bd795d565b9427b55f92dcb77
bkennedy <bkennedy@smm.org>
2018-11-15 11:53:11 -0600

    docs: Remove lerna reference
    We're not using Lerna any longer.

--- packages/server-graphql/webpack.common.js
+++ packages/server-graphql/webpack.common.js
@@ -19,16 +19,15 @@ module.exports = {
 
   //
   // Externals ensures that webpack doesn't try to compile the node_modules
-  // folder. Excluding the node_modules fixes problems with sequilize.
-  // We need an additional entry here that maps to the lerna monorepo
-  // node_modules folder, because nodeExternals won't walk the symlinks.
+  // folder. Excluding the node_modules fixes problems with Sequelize.
   //
   externals: [
     nodeExternals({ whitelist: ['webpack/hot/poll?1000'] }),
     nodeExternals({
       whitelist: ['webpack/hot/poll?1000'],
-      modulesDir: path.resolve(__dirname, '../../node_modules')
-    })],
+      modulesDir: path.resolve(__dirname, '../../node_modules'),
+    }),
+  ],
 
   //
   // Use babel to transpile ES6 code to raw node.
