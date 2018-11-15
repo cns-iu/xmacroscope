fc075553aa40e54caeed9bf871fb90de5aab56f9
bkennedy <bkennedy@smm.org>
2018-11-15 17:12:17 -0600

    style: Update service worker style
    Matching our own style rather than fb's c-r-a one, because eslint parses
    this file.

--- packages/client-run/src/registerServiceWorker.js
+++ packages/client-run/src/registerServiceWorker.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-console,no-param-reassign */
+/* eslint-disable no-param-reassign,no-console */
+
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
@@ -10,13 +11,13 @@
 // This link also includes instructions on opting out of this behavior.
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
-    ),
+  window.location.hostname === 'localhost'
+  // [::1] is the IPv6 localhost address.
+  || window.location.hostname === '[::1]'
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  || window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
+  ),
 );
 
 function registerValidSW(swUrl) {
@@ -54,8 +55,8 @@ function checkValidServiceWorker(swUrl) {
     .then((response) => {
       // Ensure service worker exists, and that we really are getting a JS file.
       if (
-        response.status === 404 ||
-        response.headers.get('content-type').indexOf('javascript') === -1
+        response.status === 404
+        || response.headers.get('content-type').indexOf('javascript') === -1
       ) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then((registration) => {
@@ -75,6 +76,14 @@ function checkValidServiceWorker(swUrl) {
     });
 }
 
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.unregister();
+    });
+  }
+}
+
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
@@ -82,7 +91,8 @@ export default function register() {
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+      // serve assets; see
+      // https://github.com/facebookincubator/create-react-app/issues/2374
       return;
     }
 
@@ -90,20 +100,22 @@ export default function register() {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Lets check if a service worker still exists or not.
+        // This is running on localhost. Lets check if a service worker still
+        // exists or not.
         checkValidServiceWorker(swUrl);
+
+        // Add some additional logging to localhost, pointing developers to the
+        // service worker/PWA documentation.
+        navigator.serviceWorker.ready.then(() => {
+          console.log(
+            'This web app is being served cache-first by a service '
+            + 'worker. To learn more, visit https://goo.gl/SC7cgQ',
+          );
+        });
       } else {
         // Is not local host. Just register service worker
         registerValidSW(swUrl);
       }
-    });
-  }
-}
-
-export function unregister() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then((registration) => {
-      registration.unregister();
     });
   }
 }
