7905b413be92d71a8f60cff6f17d70fed65046c7
Gurjaspal <gurjaspal.bedi@gmail.com>
2018-11-21 10:38:33 -0500

    legend view module changes (to make pull)

--- packages/client-mav/src/app/make-a-vis/legend-view/legend-view.module.ts
+++ packages/client-mav/src/app/make-a-vis/legend-view/legend-view.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { GraphicVariableLegendComponent } from './graphic-variable-legend/graphic-variable-legend.component';
+import { DvlFwModule } from '@dvl-fw/core';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DvlFwModule
   ],
   exports: [
     MainComponent
