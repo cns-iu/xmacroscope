4d5e8ebbe76207727b20376d7cd905d220ad924c
Bruce Herr II <bherr@indiana.edu>
2018-11-21 14:16:21 -0500

    Change environments to use COSI for its location in preparation for Phase 2 deployment

--- packages/client-mav/src/environments/environment.local.ts
+++ packages/client-mav/src/environments/environment.local.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   useHashStrategy: true,
   projectConfiguration: {
-    deploymentLocation: 'smm',
-    defaultUsState: 'IN',
+    deploymentLocation: 'cosi',
+    defaultUsState: 'OH',
     endpoint: 'http://localhost:4000/graphql'
   }
 };
