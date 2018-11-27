7cc29d2821e00978ec4ae9155499cc15621f2ca1
bkennedy <bkennedy@smm.org>
2018-11-27 17:15:15 -0600

    feat: Add basic nav bar

--- packages/website/src/App.js
+++ packages/website/src/App.js
@@ -1,35 +1,83 @@
 import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import {
+  Nav,
+  NavItem,
+  Col,
+  Container,
+  Row,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+} from 'reactstrap';
+
 import './App.css';
 
-function App() {
-  return (
-    <Container>
-      <Navbar
-        color="light"
-        light
-        expand="md"
-      >
-      </Navbar>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <Row>
-        <Col sm={12}>
-          <h1>xMacroscope.org</h1>
-          <p>Abstract - Project Description</p>
-          <p>Partners</p>
-          <p>Video of the Run experience</p>
-          <p>Footer with funding</p>
-        </Col>
-      </Row>
-      <footer className="text-center">
-        The xMacroscope project is supported by the
-        {' '}
-        <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1713567">
-          National Science Foundation award #1713567
-        </a>
-      </footer>
-    </Container>
-  );
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  toggle() {
+    const { isOpen } = this.state;
+    this.setState({
+      isOpen: !isOpen,
+    });
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    return (
+      <Container>
+        <Navbar
+          color="light"
+          light
+          expand="md"
+        >
+          <NavbarBrand href="/">xMacroscope</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse
+            isOpen={isOpen}
+            navbar
+          >
+
+            <Nav
+              className="mr-auto"
+              navbar
+            >
+              <NavItem>
+                Item
+              </NavItem>
+            </Nav>
+
+          </Collapse>
+
+        </Navbar>
+
+        <Row>
+          <Col sm={12}>
+            <h1>xMacroscope.org</h1>
+            <p>Abstract - Project Description</p>
+            <p>Partners</p>
+            <p>Video of the Run experience</p>
+            <p>Footer with funding</p>
+          </Col>
+        </Row>
+        <footer className="text-center">
+          The xMacroscope project is supported by the
+          {' '}
+          <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1713567">
+            National Science Foundation award #1713567
+          </a>
+        </footer>
+      </Container>
+    );
+  }
 }
 
 export default App;
