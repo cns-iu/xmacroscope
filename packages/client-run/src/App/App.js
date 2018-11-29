7db6c1521ab26af69a295f615e638966bc91f6ac
bkennedy <bkennedy@smm.org>
2018-11-29 14:15:56 -0600

    feat: Enable subdirectory hosting for client-run
    This sets up an basepath for the client app in production builds so that
    it can properly be hosted from a subdirectory of the express server on
    server-graphql.
    
    Also cleaning up some of the server code in the graphql-server system,
    to appropriately serve resources in development and production
    environments.
    
    Issues: #250, #251

--- packages/client-run/src/App/App.js
+++ packages/client-run/src/App/App.js
@@ -4,8 +4,13 @@ import { Container } from 'reactstrap';
 import 'bootswatch/dist/darkly/bootstrap.css';
 import AppRoutes from './AppRoutes';
 
+//
+// In production, the server-graphql express server hosts the app from the '/start' directory.
+//
+const basename = process.env.NODE_ENV === 'development' ? '/' : '/start';
+
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <div className="base-container">
       <Container fluid>
         <AppRoutes />
