b7760f74900e52f61362f86f0d0125a64967218d
bkennedy <bkennedy@smm.org>
2019-02-08 12:49:03 -0600

    refactor: Improve names of field components
    Component names should describe their functionality.

--- packages/client-run/src/Components/CustomInputHOC.js
+++ packages/client-run/src/Components/CustomInputHOC.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 // setInput function makes use of Formik setFieldValue Method
 // setFiledValue Method adds value to Formik HOC
-const CustomInputHOC = (WrappedComponentUno, WrappedComponentDoz, WrappedComponentTre) => {
+const CustomInputHOC = (FieldTitle, FieldControl, FieldError) => {
   class HOC extends React.Component {
     constructor(props) {
       super(props);
@@ -39,18 +39,18 @@ const CustomInputHOC = (WrappedComponentUno, WrappedComponentDoz, WrappedCompone
       const { value } = this.state;
       return (
         <Fragment>
-          <WrappedComponentUno
+          <FieldTitle
             value={value}
             {...this.props}
           />
-          <WrappedComponentDoz
+          <FieldControl
             value={value}
             updateInputValueConcat={this.updateInputValueConcat}
             updateInputValue={this.updateInputValue}
             clearInput={this.clearInput}
             {...this.props}
           />
-          <WrappedComponentTre
+          <FieldError
             value={value}
             {...this.props}
           />
