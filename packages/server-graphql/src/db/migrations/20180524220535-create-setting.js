3d536f03fc654343891e77bbefb6e61e73638c6b
bkennedy <bkennedy@smm.org>
2019-02-11 16:10:35 -0600

    feat: Generate activity name on database gen
    Ensure that the data tables contain the right field.
    
    Issues: #266

--- packages/server-graphql/src/db/migrations/20180524220535-create-setting.js
+++ packages/server-graphql/src/db/migrations/20180524220535-create-setting.js
@@ -28,6 +28,9 @@ module.exports = {
     longitude: {
       type: Sequelize.INTEGER,
     },
+    activityName: {
+      type: Sequelize.STRING,
+    },
     runTimeout: {
       type: Sequelize.INTEGER,
     },
