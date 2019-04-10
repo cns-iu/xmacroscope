ae2da5200b7e9206e200ba0941403aa9c8251757
Brandon <brandonwkipp@gmail.com>
2019-04-10 09:49:46 -0500

    feat: Added better logging, more robust operation checks, and more comments

--- packages/server-graphql/scripts/export.sh
+++ packages/server-graphql/scripts/export.sh
@@ -1,33 +1,50 @@
 #!/bin/sh
 set -e
 
+echo "Running export cron job..."
+
+# Check to see we've provided an argument for the db
 if [ $# -eq 0 ]
   then
     echo "DB location required."
     exit 1
 fi
 
+# Check to see that the db we've provided actually exists
 if [ ! -f $1 ]
   then
     echo "DB file does not exist."
     exit 1
 fi
 
-DB_LOCATION=$1
+# We need the actual location of db on disk because we're gonna change directories
+PWD=$(pwd)
+DB_LOCATION=$PWD/$1
+
+# This allows us to use fixed relative paths ensuring we're acting relative to the scripts directory
+SCRIPTDIR="$(dirname "$0")"
+cd "$SCRIPTDIR"
+
+# TODO: pull organization out of database or maybe from .env?
+
 DATE=`date +"%Y-%m-%d-%T"`
 LOCATION='smm'
 EXPORT_DIR="export-${DATE}-${LOCATION}"
 
+# We should already have a tmp directory, but you never know...
 mkdir -p ../tmp
 mkdir ../tmp/$EXPORT_DIR
 
-# Output organization so the other side knows who we are
+# Output location so the other side knows who we are
 echo $LOCATION > ../tmp/$EXPORT_DIR/org
 
 #sqlite3 $DB_LOCATION ".headers ON" "SELECT * FROM Messages;" >> tmp/$EXPORT_DIR/messages.sql
 #sqlite3 $DB_LOCATION ".headers ON" "SELECT * FROM People;" >> tmp/$EXPORT_DIR/people.sql
 #sqlite3 $DB_LOCATION ".headers ON" "SELECT * FROM Performances;" >> tmp/$EXPORT_DIR/performances.sql
 #sqlite3 $DB_LOCATION ".headers ON" "SELECT * FROM Runs;" >> tmp/$EXPORT_DIR/runs.sql
+
+# Get rid of SequelizeMeta table
+# sed -i '' '/SequelizeMeta/d' $TEMP_DIR/data.sql
 
 # Limiting the dump to specific tables
 sqlite3 $DB_LOCATION ".dump 'People'" ".dump 'Runs'" > ../tmp/$EXPORT_DIR/dump.sql
@@ -36,13 +53,24 @@ sqlite3 $DB_LOCATION ".schema" > ../tmp/$EXPORT_DIR/schema.sql
 # Output the diff of the dump and the schema to get rid of the CREATE statements
 grep -vx -f ../tmp/$EXPORT_DIR/schema.sql ../tmp/$EXPORT_DIR/dump.sql > ../tmp/$EXPORT_DIR/data.sql
 
-# Get rid of SequelizeMeta table
-# sed -i '' '/SequelizeMeta/d' $TEMP_DIR/data.sql
-
 # Cleanup the TEMP_DIR to tar
-rm -f ../tmp/$EXPORT_DIR/dump.sql ../tmp/$EXPORT_DIR/schema.sql
+rm -f ../tmp/$EXPORT_DIR/dump.sql
+rm -f ../tmp/$EXPORT_DIR/schema.sql
 
+# Tar up everything
 tar -zcf ../tmp/$EXPORT_DIR.tar.gz -C ../tmp/$EXPORT_DIR .
-rm -rf ../tmp/$EXPORT_DIR
 
-# TODO: rsync to deployment server
+# TODO: in the distant future, we should encrypt our tarred file to make sure nothing get compromised
+
+rm -r ../tmp/$EXPORT_DIR
+
+# TODO: pushing to deployment server
+# probably should end up using curl to push future jwt token integration instead of rsync
+
+# https://medium.com/@nieldw/using-curl-to-authenticate-with-jwt-bearer-tokens-55b7fac506bd
+# curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" https://{hostname}/api/myresource
+
+# Let someone know we were successful
+echo "OPERATION SUCCESS"
+
+# TODO: probably should send an email to some sort of logging service to report as successful
--- packages/server-graphql/scripts/import.sh
+++ packages/server-graphql/scripts/import.sh
@@ -1,65 +1,123 @@
 #!/bin/sh
 set -e
 
-if [ ! $# -eq 2 ]
+echo "Running import cron job..."
+
+# Check to see we've provided an argument for the db
+if [ $# -eq 0 ] ;
   then
-    echo "Arguments required (DB file, import file)"
+    echo "DB location required."
     exit 1
 fi
 
-if [ ! -f $1 ]
+# Check to see that the db we've provided actually exists
+if [ ! -f $1 ] ;
   then
     echo "DB file does not exist."
     exit 1
 fi
 
-if [ ! -f $2 ]
-  then
-    echo "Import file does not exist."
-    exit 1
-fi
+# We need the actual location of db on disk because we're gonna change directories
+PWD=$(pwd)
+DB_LOCATION=$PWD/$1
 
-DB_LOCATION=$1
-TEMP_FILE=$2
-TEMP_DIR=../tmp/import
+# This allows us to use fixed relative paths ensuring we're acting relative to the scripts directory
+SCRIPTDIR="$(dirname "$0")"
+cd "$SCRIPTDIR"
+
+# Let's get a list of everything we need to import sorted
+FILES=$(find ../tmp -iname '*.gz' | sort)
+
 LOG_DIR=../logs
+TEMP_DIR=../tmp/import
 
+# Should already exist, but you never know
 mkdir -p $LOG_DIR
-mkdir $TEMP_DIR && tar -zxf $TEMP_FILE -C $TEMP_DIR
 
-ORG=$(cat "$TEMP_DIR/org")
-DATE=`date +"%Y-%m-%d-%T"`
-LOG_FILE=$LOG_DIR/import-$DATE-$ORG.log
+for FILE in $FILES
+do
+  REQUIRED_FILES_PRESENT=true
+  echo "Importing $FILE"
 
-echo "------- Import from $ORG - $DATE -------\n" > $LOG_FILE
-echo "Opening sqlite..."
-sqlite3 $DB_LOCATION < $TEMP_DIR/data.sql 1>>$LOG_FILE 2>>$LOG_FILE || echo "Closing sqlite..."
+  mkdir -p $TEMP_DIR
+  tar -zxf $FILE -C $TEMP_DIR
 
-# Keeping this here for the moment in case we decide to do
-# something more granular than the sqlite cli can give us
+  DATE=`date +"%Y-%m-%d-%T"`
+  LOG_FILE=$LOG_DIR/import-$DATE.log
 
-#for query in export-2019-04-03-15:53:33-smm/*.sql ; do
-#  COLUMN_ROW=true
-#
-#  while read line ; do
-#    VALUES=
-#    QUERY="INSERT INTO table ("
-#    IFS='|' read -r -a array <<< "$line"
-#
-#    for col in "${array[@]}" ; do
-#      VALUES+="'$col',"
-#    done
-#
-#    if [ "$COLUMN_ROW" = true ] ; then
-#      COLUMN_ROW=false
-#      COLUMNS=$VALUES
-#    else
-#      QUERY+=$COLUMNS") VALUES ("
-#      QUERY+=$VALUES");"
-#
-#      echo "$QUERY" >> import.sql
-#    fi
-#  done < "$query"
-#done
+  echo "-------------------- Importing $FILE ----------------------" >> $LOG_DIR/$LOG_FILE
 
-rm -rf $TEMP_DIR $TEMP_FILE
+  # Check to make sure 'data.sql' exists
+  if [ ! -f "$TEMP_DIR/data.sql" ] ;
+    then
+      REQUIRED_FILES_PRESENT=false
+      echo "Couldn't find 'data.sql'" >> $LOG_DIR/$LOG_FILE
+  fi
+
+  # Check to make sure 'org' exists
+  if [ ! -f "$TEMP_DIR"/org ] ;
+    then
+      REQUIRED_FILES_PRESENT=false
+      echo "Couldn't find 'org'" >> $LOG_DIR/$LOG_FILE
+  fi
+
+  # If either check fails above, let's skip the import of that file altogether
+  if ! "$REQUIRED_FILES_PRESENT" ;
+    then
+      echo "Skipping import for $FILE" >> $LOG_DIR/$LOG_FILE
+      echo "OPERATION FAILED\n" >> $LOG_DIR/$LOG_FILE
+
+      # Remove the untarred directory but let's keep the original tarred file around
+      rm -r $TEMP_DIR
+
+      # TODO: we should move the failed $FILE to new directory so that we don't try to reimport?
+      # TODO: probably should send an email out to notify us that something is up
+
+      continue
+  fi
+
+  ORG=$(cat "$TEMP_DIR/org")
+
+  echo "FILE: $FILE" >> $LOG_DIR/$LOG_FILE
+  echo "ORG: $ORG\n" >> $LOG_DIR/$LOG_FILE
+  echo "Opening sqlite..." >> $LOG_DIR/$LOG_FILE
+  sqlite3 $DB_LOCATION < $TEMP_DIR/data.sql 1>>$LOG_DIR/$LOG_FILE 2>>$LOG_DIR/$LOG_FILE || echo "Closing sqlite..." >> $LOG_DIR/$LOG_FILE
+
+  # Keeping this here for the moment in case we decide to do
+  # something more granular than the sqlite cli can give us
+
+  #for query in export-2019-04-03-15:53:33-smm/*.sql ; do
+  #  COLUMN_ROW=true
+  #
+  #  while read line ; do
+  #    VALUES=
+  #    QUERY="INSERT INTO table ("
+  #    IFS='|' read -r -a array <<< "$line"
+  #
+  #    for col in "${array[@]}" ; do
+  #      VALUES+="'$col',"
+  #    done
+  #
+  #    if [ "$COLUMN_ROW" = true ] ; then
+  #      COLUMN_ROW=false
+  #      COLUMNS=$VALUES
+  #    else
+  #      QUERY+=$COLUMNS") VALUES ("
+  #      QUERY+=$VALUES");"
+  #
+  #      echo "$QUERY" >> import.sql
+  #    fi
+  #  done < "$query"
+  #done
+
+  # Clean up the mess of dirs and files we've made
+  echo "Cleaning up..." >> $LOG_DIR/$LOG_FILE
+  rm -r $TEMP_DIR
+  rm $FILE
+
+  # Let someone know it was a success
+  echo "OPERATION SUCCESS\n" >> $LOG_DIR/$LOG_FILE
+
+  # TODO: probably should send an email to some sort of logging service to report as successful
+
+done
