66e9d2a1585afc7aa4b242050799157a8d742f2b
Brandon <brandonwkipp@gmail.com>
2019-05-22 15:26:36 -0500

    refactor: Removed duplicate markup, and made footer wider

--- packages/website/src/FooterSite/index.js
+++ packages/website/src/FooterSite/index.js
@@ -1,34 +1,40 @@
 import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import { Container } from 'reactstrap';
 import nsfLogo from './nsf-logo.png';
 
 function FooterSite() {
   return (
-    <footer className="text-center">
+    <footer className="footer-body mt-0 pt-4 pb-4 text-center">
       <Container>
-        <Row className="justify-content-center">
-          <Col
-            className="pt-4 mt-4 footer-body"
-            xs={6}
-          >
-            <Row>
-              <Col xs={12} md={4} className="text-sm-center text-md-right">
-                <img
-                  className="img-fluid"
-                  src={nsfLogo}
-                  alt="placeholder"
-                />
-              </Col>
-              <Col xs={12} md={8} className="mb-3 text-sm-center text-md-left">
-                The xMacroscope project is supported by the
-                {' '}
-                <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1713567">
-                  National Science Foundation, award #1713567
-                </a>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+        <a
+          href="https://www.nsf.gov"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img
+            className="img-fluid"
+            src={nsfLogo}
+            alt="placeholder"
+          />
+        </a>
+        The xMacroscope project is supported by the
+        {' '}
+        <a
+          href="https://www.nsf.gov"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          National Science Foundation
+        </a>
+        {', '}
+        <a
+          href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1713567"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          award #1713567
+        </a>
+        .
       </Container>
     </footer>
   );
