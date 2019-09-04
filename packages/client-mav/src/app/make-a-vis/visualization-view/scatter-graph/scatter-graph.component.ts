48f7cb127ef0ac3c0a18274fdc039c2a50364220
axdanbol <daniel_bolin@yahoo.com>
2019-09-04 11:40:29 -0400

    Added buttons for (x|y)-axis, color, shape, and size

--- packages/client-mav/src/app/make-a-vis/visualization-view/scatter-graph/scatter-graph.component.ts
+++ packages/client-mav/src/app/make-a-vis/visualization-view/scatter-graph/scatter-graph.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Visualization } from '@dvl-fw/core';
+import { Component, Input } from '@angular/core';
+import { Visualization, GraphicVariableType } from '@dvl-fw/core';
 import { XMacroscopeDataService } from 'xmacroscope-dvl-fw-plugin';
 
 @Component({
   selector: 'app-scatter-graph',
   templateUrl: './scatter-graph.component.html',
-  styleUrls: ['./scatter-graph.component.sass']
+  styleUrls: ['./scatter-graph.component.scss']
 })
-export class ScatterGraphComponent implements OnInit {
-  @Input() data: Visualization; // FIXME: Change data type later.
-  constructor(public dataService: XMacroscopeDataService) {
+export class ScatterGraphComponent {
+  @Input() data: Visualization;
 
-   }
+  readonly xAxisType = GraphicVariableType.AXIS;
+  readonly yAxisType = GraphicVariableType.AXIS;
+  readonly colorType = GraphicVariableType.COLOR;
+  readonly shapeType = GraphicVariableType.SHAPE;
+  readonly sizeType = GraphicVariableType.AREA_SIZE;
 
-  ngOnInit() {
-    console.log(this.data);
-    console.log(this.data.graphicSymbols.points.graphicVariables);
-  }
-
+  constructor(public dataService: XMacroscopeDataService) { }
 }
