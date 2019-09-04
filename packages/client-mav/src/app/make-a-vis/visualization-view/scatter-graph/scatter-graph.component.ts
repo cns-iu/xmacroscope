4db3123c892ee21017479bae61a82aaa54ec9c1b
axdanbol <daniel_bolin@yahoo.com>
2019-09-04 15:16:41 -0400

    Connected buttons to variable change

--- packages/client-mav/src/app/make-a-vis/visualization-view/scatter-graph/scatter-graph.component.ts
+++ packages/client-mav/src/app/make-a-vis/visualization-view/scatter-graph/scatter-graph.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { Visualization, GraphicVariableType } from '@dvl-fw/core';
+import { Component, ContentChild, Input, ViewChild } from '@angular/core';
+import { DvlFwVisualizationComponent, GraphicVariable, GraphicVariableType, Visualization } from '@dvl-fw/core';
 import { XMacroscopeDataService } from 'xmacroscope-dvl-fw-plugin';
 
 @Component({
@@ -10,11 +10,35 @@ import { XMacroscopeDataService } from 'xmacroscope-dvl-fw-plugin';
 export class ScatterGraphComponent {
   @Input() data: Visualization;
 
+  @ViewChild('visualization', { static: true }) visualization: DvlFwVisualizationComponent;
+
   readonly xAxisType = GraphicVariableType.AXIS;
   readonly yAxisType = GraphicVariableType.AXIS;
   readonly colorType = GraphicVariableType.COLOR;
   readonly shapeType = GraphicVariableType.SHAPE;
   readonly sizeType = GraphicVariableType.AREA_SIZE;
 
-  constructor(public dataService: XMacroscopeDataService) { }
+  readonly variables: GraphicVariable[];
+  readonly defaultXAxisVariable: GraphicVariable;
+  readonly defaultYAxisVariable: GraphicVariable;
+
+  constructor(dataService: XMacroscopeDataService) {
+    this.variables = dataService.project.graphicVariables;
+    this.defaultXAxisVariable = this.findVariable(/time/i, this.xAxisType);
+    this.defaultYAxisVariable = this.findVariable(/height/i, this.yAxisType);
+    console.log(this);
+  }
+
+  variableChanged(variable: GraphicVariable, id: string): void {
+    if (this.data) {
+      this.data.graphicSymbols['points'].graphicVariables[id] = variable;
+      this.visualization.runDataChangeDetection();
+    }
+  }
+
+  private findVariable(label: RegExp, type: GraphicVariableType): GraphicVariable {
+    return this.variables.find(variable => {
+      return label.test(variable.label) && variable.type === type;
+    });
+  }
 }
