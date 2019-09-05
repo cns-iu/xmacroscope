3e0d15fc3b87f723c712c842b1da47417098554a
Bruce Herr II <bherr@indiana.edu>
2019-09-05 14:24:56 -0400

    Graphic variable changes now propagate to legend

--- packages/client-mav/src/app/make-a-vis/legend-view/main/main.component.ts
+++ packages/client-mav/src/app/make-a-vis/legend-view/main/main.component.ts
@@ -27,7 +27,8 @@ export class MainComponent implements OnInit, OnChanges {
   graphicSymbolOptions: GraphicSymbolOption;
   graphicVariableOptions: GraphicVariableOption[] =  [
     { type: 'color', label: 'Color', visualization: 'color' },
-    { type: 'areaSize', label: 'Area Size', visualization: 'node-size' }
+    { type: 'areaSize', label: 'Area Size', visualization: 'node-size' },
+    { type: 'shape', label: 'Shape', visualization: 'shape' }
   ];
 
   constructor(private dataService: XMacroscopeDataService, private updateService: UpdateVisService) {
