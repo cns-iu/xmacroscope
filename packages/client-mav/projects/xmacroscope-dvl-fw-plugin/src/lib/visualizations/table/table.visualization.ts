bb80def1d4e3c20831b93e9bbcb0b5a929b95611
axdanbol <daniel_bolin@yahoo.com>
2019-09-05 13:11:36 -0400

    Added scaffolding for table vis

---
+++ packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/visualizations/table/table.visualization.ts
@@ -0,0 +1,28 @@
+import { DefaultVisualization, ObjectFactory, Visualization, Project, ObjectFactoryRegistry } from '@dvl-fw/core';
+import { TableComponent } from './table.component';
+
+
+export class TableVisualization extends DefaultVisualization {
+  // tslint:disable-next-line: max-line-length
+  readonly defaultDescription = 'A table displaying data.';
+  readonly component = TableComponent;
+  readonly graphicSymbolOptions = [{
+    id: 'data', label: 'Data', type: 'area',
+    graphicVariableOptions: [
+      { type: 'identifier', label: 'Identifier', visualization: 'id', scaleType: 'ratio', required: true },
+      { type: 'order', label: 'Sort Order' }
+    ]
+  }];
+}
+
+export class TableVisualizationFactory implements ObjectFactory<Visualization, Project> {
+  id = 'table';
+  type = 'visualization';
+
+  async fromJSON(data: any, context: Project, registry: ObjectFactoryRegistry): Promise<Visualization> {
+    return new TableVisualization(data, context);
+  }
+  toJSON(instance: Visualization, context: Project, registry: ObjectFactoryRegistry) {
+    return instance.toJSON();
+  }
+}
--- packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/visualizations/visualizations.module.ts
+++ packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/visualizations/visualizations.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
-import { ScatterplotMapComponent } from './scatterplot-map/scatterplot-map.component';
 import { GeographicMapComponent } from './geographic-map/geographic-map.component';
+import { ScatterplotMapComponent } from './scatterplot-map/scatterplot-map.component';
+import { TableModule } from './table/table.module';
 
 
 @NgModule({
-  declarations: [ScatterplotMapComponent, GeographicMapComponent],
   imports: [
     CommonModule,
-    NgxMapboxGLModule
+    NgxMapboxGLModule,
+    TableModule
   ],
+  declarations: [ScatterplotMapComponent, GeographicMapComponent],
   entryComponents: [ScatterplotMapComponent, GeographicMapComponent]
 })
 export class VisualizationsModule { }
