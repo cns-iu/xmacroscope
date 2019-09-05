9d16a4edfd212bece94ef595684a2919c1a616d6
Bruce Herr II <bherr@indiana.edu>
2019-09-05 08:17:00 -0400

    Updated package-lock and added data urls for cheetah, turtle, and squirrel for the dvl side of the project.

--- packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/shared/run.ts
+++ packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/shared/run.ts
@@ -4,6 +4,13 @@ import { areaSizeScale } from '@dvl-fw/core';
 
 import { Person } from './person';
 
+
+const images = {
+  'cheetah': 'data:image/gif;base64,R0lGODlhZABkAIABAAAAAP///yH+EUNyZWF0ZWQgd2l0aCBHSU1QACH5BAEKAAEALAAAAABkAGQAAALpjI+py+0Po5y02ouz3rz7D4biSJbmiabqyrbuC8fyTNf2jef6zvf+DwwKh8Si8YhMKpfMpvMJjUqn1Afgis1WEdmut+u51r7kMvYClpnX7HYZ5o7L2QetaY7Pp70kvT9fl/bxRzjHxddRqBiX8JWxCOmm8GYVaSm3cKmp6bAYuOkJoZcJiplpsSZaSqez6vjjKibkSoSJV+RIZmBam8vHmAQJ9VfFuxXAevz5qnyo27yMCI1MOZ16fEtVnczEPFm9dDbhHSw7fY6err7O3u7+Dh8vP09fb3+Pn6+/z9/v/w8woMCBBAs2KQAAOw==',
+  'turtle': 'data:image/gif;base64,R0lGODlhZABkAIABAAAAAP///yH+EUNyZWF0ZWQgd2l0aCBHSU1QACH5BAEKAAEALAAAAABkAGQAAAL+jI+py+0Po5y02ouz3rz7D4biSJbmiabqyrbuC8fyTNf2jef6zvf+DwwKh8Si8YhMKpfMpgoAjRqiVKeCis1qAcyt91s1gsdjIvkMBqLXaR777dXB51sc/Z41QSX4fliUx+A3KBXylUCYyAWi2Fj24Rh56CFZqUVpmbnXodmJ2WnJCZopOipZahr5merIwRqa8XqqIasaWwsagcvqsPsq6Mt7FSyMEBdAXGlcN5U82Yy2HCjtvLmwRl2I6MxHlm09XLtxTA3hJ8N8RfF3QD6DhZJes7d4cmk14Y7/oL/f0O8v3LSA/9gR5FfvoMKFDBs6fAgxosSJFCtavIgxo8YPjRw7evwIMqTIkSRLjiwAADs=',
+  'squirrel': 'data:image/gif;base64,R0lGODlhZABkAIABAAAAAP///yH+EUNyZWF0ZWQgd2l0aCBHSU1QACH5BAEKAAEALAAAAABkAGQAAAL+jI+py+0Nopzg2YuzPrT7uIXiaHymSaaqcrboCofu/MW2RefezbP6T+n1gESJkFdMHmPJ5nLVdD5H0ehUVJVeNVnt9tL1fh1h5fhRFp8TafN60Xa/EfHi3Ff/3dl54J7Tp/dXEpgzSFg4cxiQaHhoh5i1CAlYtcjohyeXAYIkqEmE1WkTyrCJMeGpg5a5EXTzCRHL+Uq6ikMj2qGqiNqiW8N0S7sr42LrODZLMrxFmdK8ZKkSPRQGnWxd6Jur3Thqmi38XQv6i0y+wyeOnX5CV03lngc1T99uH4ef38bMXyfvH0BjAgdyKWiQGMJ+BxcydLXD4SlqEpfBqMgOHcaDYNI2cnzisRjIkOUIkoyn8GQ3YCpXmmzZiyXMd/5mHttXoVKugs5+IVwjkuckoY+I/vk5VOAlo4OYHv13KZK9qFLTUa367apOclqxbkvKD2zYokrFzutKx2satOsCsVX7EC3Xt1nfYvpqt5HdrTTvutwLNycZdYDhMCJU+IjgxIzPFAAAOw=='
+};
+
 function ifShowPersona<T>(trueAttr: string, falseAttr: string): Operator<any, T> {
   return map(s => s.showPersona ? get(s, trueAttr) : get(s, falseAttr));
 }
