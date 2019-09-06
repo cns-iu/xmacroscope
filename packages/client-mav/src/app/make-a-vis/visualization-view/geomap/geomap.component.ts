890ab8f7ce5dcf4bb27f36e0decb611bc08e2e82
axdanbol <daniel_bolin@yahoo.com>
2019-09-06 14:38:56 -0400

    Retain state for the geomap feature selection

--- packages/client-mav/src/app/make-a-vis/visualization-view/geomap/geomap.component.ts
+++ packages/client-mav/src/app/make-a-vis/visualization-view/geomap/geomap.component.ts
@@ -11,7 +11,7 @@ import { XMacroscopeDataService } from 'xmacroscope-dvl-fw-plugin';
 
 import { UpdateVisService } from '../../../shared/services/update-vis.service';
 
-export interface ButtonItems {
+export interface ButtonItem {
   label: string;
   icon: string;
   id: string;
@@ -33,7 +33,7 @@ export class GeomapComponent {
   readonly sizeType = GraphicVariableType.AREA_SIZE;
 
   readonly variables: GraphicVariable[];
-  readonly buttonItems: ButtonItems[] = [
+  readonly buttonItems: ButtonItem[] = [
     {
       label: 'United States',
       icon: 'map:us',
@@ -54,11 +54,16 @@ export class GeomapComponent {
     this.variables = dataService.project.graphicVariables;
   }
 
+  currentFeature(): ButtonItem {
+    const id = get(this.data, ['properties', 'featureSelection']);
+    return this.buttonItems.find(item => item.id === id) || this.buttonItems[1];
+  }
+
   currentVariable(id: string): GraphicVariable {
     return get(this.graphicSymbol, ['graphicVariables', id]);
   }
 
-  geoMapChanged(selectedMap: ButtonItems): void {
+  geoMapChanged(selectedMap: ButtonItem): void {
     if (this.data) {
       this.data.properties.featureSelection = selectedMap.id;
       this.visualization.runDataChangeDetection();
