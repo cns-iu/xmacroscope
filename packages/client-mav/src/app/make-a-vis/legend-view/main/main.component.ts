6412a6a6119d895746ba4f66a8c373ab191f0179
axdanbol <daniel_bolin@yahoo.com>
2019-09-06 11:48:37 -0400

    Cleaned up legened (WIP)

--- packages/client-mav/src/app/make-a-vis/legend-view/main/main.component.ts
+++ packages/client-mav/src/app/make-a-vis/legend-view/main/main.component.ts
@@ -12,106 +12,40 @@ import { XMacroscopeDataService } from 'xmacroscope-dvl-fw-plugin';
 
 import { UpdateVisService } from '../../../shared/services/update-vis.service';
 
-export interface Group {
-  option: GraphicSymbolOption;
-}
-
-export interface ButtonGroupItem {
-  label: string;
-  id: string;
-  icon: string;
-}
 
 @Component({
   selector: 'app-legend-view-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.sass']
 })
-export class MainComponent implements OnChanges {
+export class MainComponent {
   @Input() activeVisualization: number;
 
-  private lastActiveVisualization: number;
-  private lastProject: Project;
-  streams: RecordStream[];
-  project: Project;
-  visualization: Visualization;
-  graphicSymbolOptions: GraphicSymbolOption;
-  graphicVariableOptions: GraphicVariableOption[] =  [
-    { type: 'color', label: 'Color', visualization: 'color' },
-    { type: 'areaSize', label: 'Area Size', visualization: 'node-size' },
-    { type: 'shape', label: 'Shape', visualization: 'shape' }
+  readonly navigation = [
+    { label: 'Data Table', id: 'datatable', icon: 'visualization:table' },
+    { label: 'Scatter Graph', id: 'scattergraph', icon: 'visualization:scatter-graph' },
+    { label: 'Map', id: 'geomap', icon: 'visualization:geomap' }
+  ];
+  readonly legends = [
+    { variable: 'color', label: 'Color', icon: 'label:color', type: 'color-legend' },
+    { variable: 'shape', label: 'Shape', icon: 'label:shape', type: 'shape-legend' },
+    { variable: 'areaSize', label: 'Size', icon: 'label:size', type: 'size-legend' }
   ];
 
-  navigationButtons: ButtonGroupItem[] = [
-    {
-      label: 'Data Table',
-      id: 'datatable',
-      icon: 'visualization:table'
-    },
-    {
-      label: 'Scatter Graph',
-      id: 'scattergraph',
-      icon: 'visualization:scatter-graph'
-    },
-    {
-      label: 'Map',
-      id: 'geomap',
-      icon: 'visualization:geomap'
-    }
-  ];
+  project: Project;
 
   constructor(dataService: XMacroscopeDataService, private updateService: UpdateVisService) {
     this.project = dataService.project;
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if ('activeVisualization' in changes) {
-      const changed = this.activeVisualization !== this.lastActiveVisualization || this.project !== this.lastProject;
-      this.lastActiveVisualization = this.activeVisualization;
-      this.lastProject = this.project;
-      if (changed) {
-        this.setState(this.project, this.activeVisualization - 1);
-      }
-    }
-  }
-
-  onGraphicVariableChange(group: any, option: GraphicVariableOption, gv: GraphicVariable): void {
-    const  { updateService, visualization } = this;
-    const id = option.id || option.type;
-    if (visualization) {
-      updateService.updateGraphicVariable(visualization, group.id, id, gv);
-    }
-  }
-
-  navigationChanged(event: any) {} // FIXME
-
-  private setState(project: Project, index: number): void {
-    const visualization = this.visualization = project && index >= 0 && project.visualizations[index];
-    if (project) {
-      this.setStreams(project);
-      if (visualization) {
-        this.setGroups(visualization);
-      }
-    }
-  }
-
-  private setStreams(project: Project): void {
-    this.streams = project.dataSources
-      .map(source => source.recordStreams)
-      .reduce((acc, s) => acc.concat(s), [] as RecordStream[]);
-  }
-
-  private setGroups(visualization: Visualization): void {
-    const graphicSymbols = cloneDeep(visualization.graphicSymbols);
-    Object.keys(graphicSymbols).forEach((key) => {
-      this.graphicSymbolOptions = visualization.graphicSymbolOptions.filter((option) => {
-        return option.id === key;
-      })[0];
-    });
   }
 
   isVariableFixed(type: string): boolean {
     const id = get(this.project, ['graphicSymbols', 0, 'graphicVariables', type, 'id']);
     return id === 'fixed';
   }
+
+  variableLabel(type: string): string {
+    return get(this.project, ['graphicSymbols', 0, 'graphicVariables', type, 'label']);
+  }
+
+  navigationChanged(event: any) {} // FIXME
 }
