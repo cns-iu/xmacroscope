ad2ff75b1d32bd551acbd432bc4c7dcd5cbab974
shashank shekhar <shashankshekhar@149-161-156-221.dhcp-bl.indiana.edu>
2019-09-09 13:33:47 -0400

    removed xmacroscope persona

--- packages/client-mav/src/app/shared/shared.module.ts
+++ packages/client-mav/src/app/shared/shared.module.ts
@@ -3,19 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { ButtonGroupModule } from './components/button-group';
 import { GVButtonGroupModule } from './components/gv-button-group';
-import { XMacroscopePersonaComponent } from './components/xmacroscope-persona/xmacroscope-persona.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ButtonGroupModule,
     GVButtonGroupModule
-  ],
-  declarations: [
-    XMacroscopePersonaComponent
-  ],
-  exports: [
-    XMacroscopePersonaComponent
   ]
 })
 export class SharedModule { }
