bdda58a054e64b4c43d8fbab4cb92f4f3fe572bc
Bruce Herr II <bherr@indiana.edu>
2020-02-11 13:23:16 -0500

    Finally got the order right

--- packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/shared/opponent-runs.ts
+++ packages/client-mav/projects/xmacroscope-dvl-fw-plugin/src/lib/shared/opponent-runs.ts
@@ -8,24 +8,9 @@ const moment = moment_; // See https://github.com/jvandemo/generator-angular2-li
 function getOpponentRuns(location: string): any[] {
   const opponentRunData = [
     {
-      id: 'cheetah-run',
-      start: moment().add(1, 'year').toDate(),
-      end: moment().add(1, 'year').add(0.37, 'seconds').toDate(),
-      pinned: true,
-      person: {
-        id: 'cheetah', icon: 'cheetah', age: 5, height: 36, opponent: 'Visitor', shoes: 'Barefoot'
-      },
-      locations: {
-        'Indiana': { zipCode: '46202', state: 'IN', latitude: 39.7739, longitude: -86.1763 },
-        'Minnesota': { zipCode: '47907', state: 'IN', latitude: 40.4237, longitude: -86.9212 },
-        'Ohio': { zipCode: '45220', state: 'OH', latitude: 39.1433, longitude: -84.5090 },
-        'California': { zipCode: '47907', state: 'IN', latitude: 40.4237, longitude: -86.9212 }
-      }
-    },
-    {
       id: 'penguin-run',
-      start: moment().add(2, 'year').toDate(),
-      end: moment().add(2, 'year').add(5.48, 'seconds').toDate(),
+      start: moment().add(1, 'year').toDate(),
+      end: moment().add(1, 'year').add(5.48, 'seconds').toDate(),
       pinned: true,
       person: {
         id: 'penguin', icon: 'penguin', age: 13, height: 35, opponent: 'Visitor', shoes: 'Barefoot'
@@ -39,8 +24,8 @@ function getOpponentRuns(location: string): any[] {
     },
     {
       id: 'squirrel-run',
-      start: moment().add(3, 'year').toDate(),
-      end: moment().add(3, 'year').add(2.20, 'seconds').toDate(),
+      start: moment().add(2, 'year').toDate(),
+      end: moment().add(2, 'year').add(2.20, 'seconds').toDate(),
       pinned: true,
       person: {
         id: 'squirrel', icon: 'squirrel', age: 3, height: 11, opponent: 'Visitor', shoes: 'Barefoot'
@@ -49,6 +34,21 @@ function getOpponentRuns(location: string): any[] {
         'Indiana': { zipCode: '47405', state: 'IN', latitude: 39.1727, longitude: -86.5222 },
         'Minnesota': { zipCode: '47907', state: 'IN', latitude: 40.4237, longitude: -86.9212 },
         'Ohio': { zipCode: '43215', state: 'OH', latitude: 39.959836, longitude: -83.007020 },
+        'California': { zipCode: '47907', state: 'IN', latitude: 40.4237, longitude: -86.9212 }
+      }
+    },
+    {
+      id: 'cheetah-run',
+      start: moment().add(3, 'year').toDate(),
+      end: moment().add(3, 'year').add(0.37, 'seconds').toDate(),
+      pinned: true,
+      person: {
+        id: 'cheetah', icon: 'cheetah', age: 5, height: 36, opponent: 'Visitor', shoes: 'Barefoot'
+      },
+      locations: {
+        'Indiana': { zipCode: '46202', state: 'IN', latitude: 39.7739, longitude: -86.1763 },
+        'Minnesota': { zipCode: '47907', state: 'IN', latitude: 40.4237, longitude: -86.9212 },
+        'Ohio': { zipCode: '45220', state: 'OH', latitude: 39.1433, longitude: -84.5090 },
         'California': { zipCode: '47907', state: 'IN', latitude: 40.4237, longitude: -86.9212 }
       }
     },
