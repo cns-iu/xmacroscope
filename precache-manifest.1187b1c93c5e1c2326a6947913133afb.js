11b417727ba53bfc6e3169a1e642e2c1c4782e3a
bherr2 <bherr2@users.noreply.github.com>
2021-09-23 19:49:37 +0000

    Staging deploy -- 9dad7880394f23f8427a69a36c7825f53b29faff

---
+++ precache-manifest.1187b1c93c5e1c2326a6947913133afb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "67aece03573dc468e5d4",
+    "url": "/xmacroscope/static/css/main.a40ed993.chunk.css"
+  },
+  {
+    "revision": "67aece03573dc468e5d4",
+    "url": "/xmacroscope/static/js/main.67aece03.chunk.js"
+  },
+  {
+    "revision": "dde529f111802ad60f4b",
+    "url": "/xmacroscope/static/css/1.c91bf87b.chunk.css"
+  },
+  {
+    "revision": "dde529f111802ad60f4b",
+    "url": "/xmacroscope/static/js/1.dde529f1.chunk.js"
+  },
+  {
+    "revision": "561404130540ae9db836",
+    "url": "/xmacroscope/static/js/runtime~main.56140413.js"
+  },
+  {
+    "revision": "d9ea5e7799df8a5dc67055ff39de39d1",
+    "url": "/xmacroscope/static/media/smm-horizontal.d9ea5e77.png"
+  },
+  {
+    "revision": "d07c3cdb55e4f66a2109cc9f96b0bb06",
+    "url": "/xmacroscope/static/media/nysci.d07c3cdb.png"
+  },
+  {
+    "revision": "add8a934bac937f3ebbd6e9dce036a77",
+    "url": "/xmacroscope/static/media/nsf-logo.add8a934.png"
+  },
+  {
+    "revision": "6735ed239d03c4885edfe207b09c132e",
+    "url": "/xmacroscope/index.html"
+  }
+];
