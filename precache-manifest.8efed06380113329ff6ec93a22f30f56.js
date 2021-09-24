369a8f67a4792292b0607710534c19b8fe1564e7
bherr2 <bherr2@users.noreply.github.com>
2021-09-24 13:27:38 +0000

    Staging deploy -- 0852567121b2a9a5ff5bfa8405d60a20e92224a8

---
+++ precache-manifest.8efed06380113329ff6ec93a22f30f56.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfbe6d239fac47d0c948",
+    "url": "/static/css/main.a40ed993.chunk.css"
+  },
+  {
+    "revision": "bfbe6d239fac47d0c948",
+    "url": "/static/js/main.bfbe6d23.chunk.js"
+  },
+  {
+    "revision": "dde529f111802ad60f4b",
+    "url": "/static/css/1.c91bf87b.chunk.css"
+  },
+  {
+    "revision": "dde529f111802ad60f4b",
+    "url": "/static/js/1.dde529f1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9ea5e7799df8a5dc67055ff39de39d1",
+    "url": "/static/media/smm-horizontal.d9ea5e77.png"
+  },
+  {
+    "revision": "d07c3cdb55e4f66a2109cc9f96b0bb06",
+    "url": "/static/media/nysci.d07c3cdb.png"
+  },
+  {
+    "revision": "add8a934bac937f3ebbd6e9dce036a77",
+    "url": "/static/media/nsf-logo.add8a934.png"
+  },
+  {
+    "revision": "e3e2531ab06113c3345a63aa43e2a107",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/xmacroscope/precache-manifest.1187b1c93c5e1c2326a6947913133afb.js"
+  "/precache-manifest.8efed06380113329ff6ec93a22f30f56.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/xmacroscope/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
