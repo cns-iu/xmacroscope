bc27aacfaf96a8e806892cb0fb4165b9be6b88f4
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-09-24 14:59:54 -0400

    Lint errors

--- packages/site-visualizations/src/assets/pages/vis3-scatterplot/scatterplot.vl.ts
+++ packages/site-visualizations/src/assets/pages/vis3-scatterplot/scatterplot.vl.ts
@@ -71,14 +71,14 @@ export function createScatterplot(visControls: VisControls): VisualizationSpec {
         sort: ['', 'Retired', 'Adult', 'Teen', 'Pre-Teen', 'Kid']
       },
       tooltip: [
-        {field: 'id', title: 'ID', type: 'nominal'},
-        {field: 'Event', title: 'Event', type: 'nominal'},
-        {field: 'Age Group', title: 'Age Group', type: 'nominal'},
-        {field: 'Gender', title: 'Gender', type: 'nominal'},
-        {field: 'Height', title: 'Height (inches)', type: 'quantitative'},
-        {field: 'state', title: 'State', type: 'nominal'},
-        {field: 'Favorite Activity', title: 'Favorite Activity', type: 'nominal'},
-        {field: 'Time (seconds)', title: 'Time (seconds)', type: 'quantitative'}
+        { field: 'id', title: 'ID', type: 'nominal' },
+        { field: 'Event', title: 'Event', type: 'nominal' },
+        { field: 'Age Group', title: 'Age Group', type: 'nominal' },
+        { field: 'Gender', title: 'Gender', type: 'nominal'},
+        { field: 'Height', title: 'Height (inches)', type: 'quantitative' },
+        { field: 'state', title: 'State', type: 'nominal'},
+        { field: 'Favorite Activity', title: 'Favorite Activity', type: 'nominal' },
+        { field: 'Time (seconds)', title: 'Time (seconds)', type: 'quantitative' }
       ],
       fill: { field: 'Event', type: 'nominal' },
       shape: visControls.shape ? { field: visControls.shape, type: 'nominal', legend: { title: visControls.shape === 'Gender' ? 'Gender' : visControls.shape } } : { value: 'circle' },
