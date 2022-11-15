54dc7b36ae698874eebf0ee3fe4a850bc7159f56
bherr2 <bherr2@users.noreply.github.com>
2022-11-15 18:09:45 +0000

    Production deploy -- 49a672c0bce26731f104df51ec908d1e29c06163

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/xmacroscope/precache-manifest.d8ab98e0f141c4f2d4bff62e9ba46e6f.js"
+  "/precache-manifest.a0b6c39226671dad059f4131c97ee41c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/xmacroscope/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
